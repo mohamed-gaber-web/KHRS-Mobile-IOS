f68cfa6373d00550dc3b4baa5afc446ad6a4c43f
Mahmoud Saad <mahmoud.saad@sdex.dk>
2021-05-04 13:35:00 +0200

    Adding auth & storage service and is related information

---
+++ src/app/api.constants.ts
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://api.emzady.com';
+
+//Account
+export const login = `${baseUrl}/api/Account/Login`;
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+
+import { HttpClient } from "@angular/common/http";
+import {loginCredentials} from "src/app/shared/models/loginCredentials";
+import { StorageService } from "src/app/shared/services/storage.service";
+import { baseUrl } from "src/app/api.constants";
+
+
+
+
+@Injectable({
+    providedIn: 'root',
+  })
+
+  export class AuthService {
+
+  constructor(private http: HttpClient, private storageService: StorageService ) {}
+
+  get IsLoggedIn() {
+    return this.storageService.existsStorage('user'); 
+  }
+
+  signInUser(user: loginCredentials) {
+    return this.http.post(`${baseUrl}`, user);
+  }
+
+  getUser() {
+    if (this.storageService.existsStorage('user')) {
+      return this.storageService.getUser();
+    }
+  }
+
+//   registerCustomer(user: RegisterCustomer) {
+//     return this.http.post(`${userRegister}`, user);
+//   }
+
+}
