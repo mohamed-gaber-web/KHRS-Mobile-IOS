1c4329e783b7996a5d78e273148f1be5fb35f59c
Mahmoud Saad <mahmoud.saad@sdex.dk>
2021-05-18 15:22:11 +0200

    fix merge

--- src/app/api.constants.ts
+++ src/app/api.constants.ts
@@ -3,15 +3,12 @@ export const imagesBaseUrl = 'https://khrs-admin.sdex.online';
 
 //Account
 export const loginAPI = `${baseUrl}/api/Account/Login`;
-<<<<<<< HEAD
 export const registerAPI = `${baseUrl}/api/Account/Register`;
 export const recommendedBy = `${baseUrl}/api/RecommendedBy/GetRecommendedBy`;
 export const userChangePassword = `${baseUrl}/api/Account/changePasswod`
 
 // get language
 export const getLanguage = `${baseUrl}/api/Language/GetLanguage`
-=======
 
 //Courses
 export const getAllCoursesAPI = `${baseUrl}/api/Course/GetCourses`;
->>>>>>> bf7d2f0a29b741707e1214d2438091d769b1ee17
