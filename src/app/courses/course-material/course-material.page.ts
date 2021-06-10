1d840f6ee7cb9b022bd33cdea9e24019348aea43
Mahmoud Saad <mahmoud.saad@sdex.dk>
2021-06-10 18:06:21 +0200

    use seek in course material

--- src/app/courses/course-material/course-material.page.ts
+++ src/app/courses/course-material/course-material.page.ts
@@ -6,8 +6,8 @@ import { switchMap } from 'rxjs/operators';
 import { CourseService } from 'src/app/shared/services/courses.service';
 import { StorageService } from 'src/app/shared/services/storage.service';
 import { AuthService } from 'src/app/auth/auth.service';
-import { IonSlides } from '@ionic/angular';
-
+import { IonRange, IonSlides } from '@ionic/angular';
+import { Howl } from 'howler';
 
 @Component({
   selector: 'app-course-material',
@@ -15,77 +15,132 @@ import { IonSlides } from '@ionic/angular';
   styleUrls: ['./course-material.page.scss'],
 })
 export class CourseMaterialPage implements OnInit {
-
   courseDetails: any;
+  player: Howl = null;
+  progress: number = 0;
+  isPlaying: boolean = false;
   subs: Subscription[] = [];
   isLoading = false;
   courseMaterial: any;
   userInfo: any;
   disablePrevBtn = true;
   disableNextBtn = false;
-
+  activeTrack: string;
   @ViewChild('slides') slides: IonSlides;
-
+  @ViewChild('range', { static: false }) range: IonRange;
 
   slideOpts = {
     initialSlide: 0,
     speed: 400,
     slidesPerView: 1,
-    scrollbar: true
+    scrollbar: true,
   };
 
   constructor(
     private router: Router,
     private courseService: CourseService,
     private route: ActivatedRoute,
-    public storageService: StorageService, public authService: AuthService
-  ) { }
+    public storageService: StorageService,
+    public authService: AuthService
+  ) {}
 
   ngOnInit() {
-
     // ** user info
     this.userInfo = this.authService.getUser();
 
     this.isLoading = true;
     this.subs.push(
-      this.route.paramMap.pipe(
-        switchMap((params: ParamMap) =>
-            this.courseService.getCourseMaterial(+params.get('courseId'), 0, 10))
-          ).subscribe(response => {
-            this.isLoading = false;
-            this.courseMaterial = response['result'];
-            // console.log(this.courseMaterial);
-            // console.log(this.slides.ionSlideNextStart)
-
-      })
+      this.route.paramMap
+        .pipe(
+          switchMap((params: ParamMap) =>
+            this.courseService.getCourseMaterial(+params.get('courseId'), 0, 10)
+          )
+        )
+        .subscribe((response) => {
+          this.isLoading = false;
+          this.courseMaterial = response['result'];
+          // console.log(this.courseMaterial);
+          // console.log(this.slides.ionSlideNextStart)
+        })
     );
-
   }
 
   // ** Move to Next slide
   slideNext() {
-    this.slides.slideNext()
+    if (this.player) {
+      this.activeTrack = null;
+      this.player.unload();
+      this.player = null;
+    }
+    this.slides.slideNext();
   }
 
   // ** Move to previous slide
   slidePrev() {
+    if (this.player) {
+      this.activeTrack = null;
+      this.player.unload();
+      this.player = null;
+    }
     this.slides.slidePrev();
   }
 
+  startAudio(voicePath: string) {
+    if (this.player) {
+      this.player.stop();
+    }
+    this.player = new Howl({
+      html5: true,
+      src: voicePath,
+      onplay: () => {
+        this.activeTrack = voicePath;
+        this.isPlaying = true;
+        this.updateProgress();
+      },
+      onend: () => {},
+    });
+    this.player.play();
+  }
+
+  toogleAudio(pause) {
+    this.isPlaying = !pause;
+    if (pause) {
+      this.player.pause();
+    } else {
+      this.player.play();
+    }
+  }
+
+  seek() {
+    if(this.player){
+      let newValue = +this.range.value;
+      let duration = this.player.duration();
+      this.player.seek(duration * (newValue / 100));
+    }
+    
+  }
+  updateProgress() {
+    if(this.player){
+    let seek = this.player.seek();
+    this.progress = (seek / this.player.duration()) * 100 || 0;
+    setTimeout(() => {
+      this.updateProgress();
+    }, 1000);
+  }
+  }
   ngOnDestroy(): void {
     this.subs.forEach((element) => {
       element.unsubscribe();
-    })
-    }
+    });
+  }
 
-    doCheck() {
-      let prom1 = this.slides.isBeginning();
-      let prom2 = this.slides.isEnd();
+  doCheck() {
+    let prom1 = this.slides.isBeginning();
+    let prom2 = this.slides.isEnd();
 
-      Promise.all([prom1, prom2]).then((data) => {
-        data[0] ? this.disablePrevBtn = true : this.disablePrevBtn = false;
-        data[1] ? this.disableNextBtn = true : this.disableNextBtn = false;
-      });
-    }
-
+    Promise.all([prom1, prom2]).then((data) => {
+      data[0] ? (this.disablePrevBtn = true) : (this.disablePrevBtn = false);
+      data[1] ? (this.disableNextBtn = true) : (this.disableNextBtn = false);
+    });
+  }
 }
