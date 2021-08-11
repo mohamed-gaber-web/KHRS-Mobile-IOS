af44ad1e3a7986f673101a6973d653dea703ae8c
mohamed jaber <mgaberweb@gmail.com>
2021-08-11 11:58:31 +0200

    add links social media

--- src/app/courses/choose-course-material/choose-course-material.page.ts
+++ src/app/courses/choose-course-material/choose-course-material.page.ts
@@ -40,7 +40,6 @@ export class ChooseCourseMaterialPage implements OnInit {
     this.subs.push(
       this.courseService.getUserCoursesDetails(this.courseId)
       .subscribe(response => {
-        console.log('choose cousre material', response)
         this.isLoading = false;
         this.userCourseDetails = response['result'].userCourse;
         let startDate = new Date(this.userCourseDetails['startDate']);
@@ -53,7 +52,7 @@ export class ChooseCourseMaterialPage implements OnInit {
       .subscribe(response => {
         this.isLoading = false;
         this.CourseDetails = response['result'];
-        console.log(this.CourseDetails)
+        console.log('course details', this.CourseDetails)
       })
     );
 
