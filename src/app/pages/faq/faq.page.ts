56a9639e31f79b3556af173459a8211461466eb7
mohamed jaber <mgaberweb@gmail.com>
2021-08-15 15:38:32 +0200

    fff

---
+++ src/app/pages/faq/faq.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { AppService } from 'src/app/shared/services/app.service';
+import { StorageService } from 'src/app/shared/services/storage.service';
+
+@Component({
+  selector: 'app-faq',
+  templateUrl: './faq.page.html',
+  styleUrls: ['./faq.page.scss'],
+})
+export class FaqPage implements OnInit {
+
+  userInfo: any;
+  panelOpenState = false;
+  offset: number = 0;
+  limit: number = 10;
+  allFaq: any;
+
+
+  constructor(
+    private authService: AuthService,
+    public storageService: StorageService,
+    private appServ: AppService, ) { }
+
+  ngOnInit() {
+    this.userInfo = this.authService.getUser();
+    this.appServ.getFaqPage(this.offset, this.limit)
+    .subscribe(response => {
+      console.log(response['result'][0].faqTranslations);
+      this.allFaq = response['result'][0].faqTranslations;
+    })
+  }
+
+}
